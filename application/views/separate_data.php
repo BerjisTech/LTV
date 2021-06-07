c8a86c80947f6d4abff4e29b75ff6051d06ed73a
berjis <berjistechnologies@gmail.com>
2021-06-07 06:44:49 +0300

    API works!

--- application/views/separate_data.php
+++ application/views/separate_data.php
@@ -1,156 +1,156 @@
 <script>
-    let revenue_data = [<?php for ($m = 30; $m > -1; $m--) :
-                            $nowmonth = strtotime(date('d-M-Y', strtotime('-' . $m . ' days')));
-                            $lastmonth = strtotime(date('d-M-Y', strtotime('-' . ($m - 1) . ' days'))); ?> {
-                y: '<?php echo date('Y-m-d', strtotime('-' . $m . ' days')); ?>',
-                a: <?php
-                            $where = "`app_id` = $app_id AND `recorded` BETWEEN '" . $nowmonth . "' AND '" . $lastmonth . "'";
-                            $shown = $this->db->where($where)->get('quaterly')->row()->last_30_days;
-                            if ($shown == '') {
-                                echo '0';
-                            } else {
-                                echo $shown;
-                            }
-                    ?>
-            },
-        <?php endfor; ?>
-    ];
-
-    let user_data = [<?php for ($m = 30; $m > -1; $m--) :
-                            $nowmonth = strtotime(date('d-M-Y', strtotime('-' . $m . ' days')));
-                            $lastmonth = strtotime(date('d-M-Y', strtotime('-' . ($m - 1) . ' days'))); ?> {
-                y: '<?php echo date('Y-m-d', strtotime('-' . $m . ' days')); ?>',
-                a: <?php
-                            $where = "`app_id` = $app_id AND `date` BETWEEN '" . $nowmonth . "' AND '" . $lastmonth . "'";
-
-                            $installed = $this->db->where('event', 'installed')->where($where)->get('shopify_data')->num_rows();
-                            $reactivated = $this->db->where('event', 'reactivated')->where($where)->get('shopify_data')->num_rows();
-                            $uninstalled = $this->db->where('event', 'uninstalled')->where($where)->get('shopify_data')->num_rows();
-                            $deactivated = $this->db->where('event', 'deactivated')->where($where)->get('shopify_data')->num_rows();
-
-                            $total = (($installed + $reactivated) - ($uninstalled + $deactivated));
-                    ?>
-            },
-        <?php endfor; ?>
-    ];
-
-    let installs_data = [<?php for ($m = 30; $m > 0; $m--) :
+    jQuery(document).ready(function($) {
+        let revenue_data = [<?php for ($m = 30; $m > -1; $m--) :
                                 $nowmonth = strtotime(date('d-M-Y', strtotime('-' . $m . ' days')));
                                 $lastmonth = strtotime(date('d-M-Y', strtotime('-' . ($m - 1) . ' days'))); ?> {
-                y: '<?php echo date('Y-m-d', strtotime('-' . $m . ' days')); ?>',
-                a: <?php
+                    y: '<?php echo date('Y-m-d', strtotime('-' . $m . ' days')); ?>',
+                    a: <?php
+                                $where = "`app_id` = $app_id AND `recorded` BETWEEN '" . $nowmonth . "' AND '" . $lastmonth . "'";
+                                $shown = $this->db->where($where)->get('quaterly')->row()->last_30_days;
+                                if ($shown == '') {
+                                    echo '0';
+                                } else {
+                                    echo $shown;
+                                }
+                        ?>
+                },
+            <?php endfor; ?>
+        ];
+
+        let user_data = [<?php for ($m = 30; $m > -1; $m--) :
+                                $nowmonth = strtotime(date('d-M-Y', strtotime('-' . $m . ' days')));
+                                $lastmonth = strtotime(date('d-M-Y', strtotime('-' . ($m - 1) . ' days'))); ?> {
+                    y: '<?php echo date('Y-m-d', strtotime('-' . $m . ' days')); ?>',
+                    a: <?php
                                 $where = "`app_id` = $app_id AND `date` BETWEEN '" . $nowmonth . "' AND '" . $lastmonth . "'";
 
                                 $installed = $this->db->where('event', 'installed')->where($where)->get('shopify_data')->num_rows();
                                 $reactivated = $this->db->where('event', 'reactivated')->where($where)->get('shopify_data')->num_rows();
-
-                                $total = ($installed + $reactivated);
-                    ?>
-            },
-        <?php endfor; ?>
-    ];
-
-    let uninstalls_data = [<?php for ($m = 30; $m > 0; $m--) :
-                                $nowmonth = strtotime(date('d-M-Y', strtotime('-' . $m . ' days')));
-                                $lastmonth = strtotime(date('d-M-Y', strtotime('-' . ($m - 1) . ' days'))); ?> {
-                y: '<?php echo date('Y-m-d', strtotime('-' . $m . ' days')); ?>',
-                a: <?php
-                                $where = "`app_id` = $app_id AND `date` BETWEEN '" . $nowmonth . "' AND '" . $lastmonth . "'";
-
                                 $uninstalled = $this->db->where('event', 'uninstalled')->where($where)->get('shopify_data')->num_rows();
                                 $deactivated = $this->db->where('event', 'deactivated')->where($where)->get('shopify_data')->num_rows();
 
-                                $total = ($uninstalled + $deactivated);
-                    ?>
-            },
-        <?php endfor; ?>
-    ];
+                                $total = (($installed + $reactivated) - ($uninstalled + $deactivated));
+                        ?>
+                },
+            <?php endfor; ?>
+        ];
 
-    let churn_data = [<?php for ($m = 30; $m > 0; $m--) :
-                            $nowmonth = strtotime(date('d-M-Y', strtotime('-' . $m . ' days')));
-                            $lastmonth = strtotime(date('d-M-Y', strtotime('-' . ($m - 1) . ' days'))); ?> {
-                y: '<?php echo date('Y-m-d', strtotime('-' . $m . ' days')); ?>',
-                a: <?php
-                            $in_where = "`app_id` = $app_id AND `date` BETWEEN '" . $nowmonth . "' AND '" . $lastmonth . "'";
+        let installs_data = [<?php for ($m = 30; $m > 0; $m--) :
+                                    $nowmonth = strtotime(date('d-M-Y', strtotime('-' . $m . ' days')));
+                                    $lastmonth = strtotime(date('d-M-Y', strtotime('-' . ($m - 1) . ' days'))); ?> {
+                    y: '<?php echo date('Y-m-d', strtotime('-' . $m . ' days')); ?>',
+                    a: <?php
+                                    $where = "`app_id` = $app_id AND `date` BETWEEN '" . $nowmonth . "' AND '" . $lastmonth . "'";
 
-                            $installed = $this->db->where('event', 'installed')->where($where)->get('shopify_data')->num_rows();
-                            $reactivated = $this->db->where('event', 'reactivated')->where($where)->get('shopify_data')->num_rows();
-                            $uninstalled = $this->db->where('event', 'uninstalled')->where($where)->get('shopify_data')->num_rows();
-                            $deactivated = $this->db->where('event', 'deactivated')->where($where)->get('shopify_data')->num_rows();
+                                    $installed = $this->db->where('event', 'installed')->where($where)->get('shopify_data')->num_rows();
+                                    $reactivated = $this->db->where('event', 'reactivated')->where($where)->get('shopify_data')->num_rows();
 
-                            $lost = ($uninstalled + $deactivated);
-                            $gained = ($installed + $reactivated);
+                                    $total = ($installed + $reactivated);
+                        ?>
+                },
+            <?php endfor; ?>
+        ];
 
-                            if ($lost == '' || $lost == 0 || $gained == '' || $gained == 0) {
-                                $total = 0;
-                            } else {
-                                $total = (($lost / $gained) * 100);
-                            }
-                            echo number_format($total);
-                    ?>
-            },
-        <?php endfor; ?>
-    ];
+        let uninstalls_data = [<?php for ($m = 30; $m > 0; $m--) :
+                                    $nowmonth = strtotime(date('d-M-Y', strtotime('-' . $m . ' days')));
+                                    $lastmonth = strtotime(date('d-M-Y', strtotime('-' . ($m - 1) . ' days'))); ?> {
+                    y: '<?php echo date('Y-m-d', strtotime('-' . $m . ' days')); ?>',
+                    a: <?php
+                                    $where = "`app_id` = $app_id AND `date` BETWEEN '" . $nowmonth . "' AND '" . $lastmonth . "'";
 
-    let reviews_data = [<?php for ($m = 7; $m > 0; $m--) :
-                            $nowmonth = strtotime(date('d-M-Y', strtotime('-' . $m . ' days')));
-                            $lastmonth = strtotime(date('d-M-Y', strtotime('-' . ($m - 1) . ' days'))); ?> {
-                y: '<?php echo date('Y-m-d', strtotime('-' . $m . ' days')); ?>',
-                a: <?php
-                            $where = "`app_id` = $app_id AND `rating` = 5 AND `review_date` BETWEEN '" . $nowmonth . "' AND '" . $lastmonth . "'";
-                            $reviews = $this->db->where($where)->get('reviews')->num_rows();
-                            echo $reviews;
-                    ?>,
-                b: <?php
-                            $where = "`app_id` = $app_id AND `rating` = 4 AND `review_date` BETWEEN '" . $nowmonth . "' AND '" . $lastmonth . "'";
-                            $reviews = $this->db->where($where)->get('reviews')->num_rows();
-                            echo $reviews;
-                    ?>,
-                c: <?php
-                            $where = "`app_id` = $app_id AND `rating` = 3 AND `review_date` BETWEEN '" . $nowmonth . "' AND '" . $lastmonth . "'";
-                            $reviews = $this->db->where($where)->get('reviews')->num_rows();
-                            echo $reviews;
-                    ?>,
-                d: <?php
-                            $where = "`app_id` = $app_id AND `rating` = 2 AND `review_date` BETWEEN '" . $nowmonth . "' AND '" . $lastmonth . "'";
-                            $reviews = $this->db->where($where)->get('reviews')->num_rows();
-                            echo $reviews;
-                    ?>,
-                e: <?php
-                            $where = "`app_id` = $app_id AND `rating` = 1 AND `review_date` BETWEEN '" . $nowmonth . "' AND '" . $lastmonth . "'";
-                            $reviews = $this->db->where($where)->get('reviews')->num_rows();
-                            echo $reviews;
-                    ?>
-            },
-        <?php endfor; ?>
-    ];
+                                    $uninstalled = $this->db->where('event', 'uninstalled')->where($where)->get('shopify_data')->num_rows();
+                                    $deactivated = $this->db->where('event', 'deactivated')->where($where)->get('shopify_data')->num_rows();
 
-    let revenue_keys = ['a']
-    let revenue_labels = ['Revenue']
-    let revenue_colors = ['#1D47F1']
+                                    $total = ($uninstalled + $deactivated);
+                        ?>
+                },
+            <?php endfor; ?>
+        ];
 
-    let user_keys = ['a']
-    let user_labels = ['User Growth']
-    let user_colors = ['#C90100']
+        let churn_data = [<?php for ($m = 30; $m > 0; $m--) :
+                                $nowmonth = strtotime(date('d-M-Y', strtotime('-' . $m . ' days')));
+                                $lastmonth = strtotime(date('d-M-Y', strtotime('-' . ($m - 1) . ' days'))); ?> {
+                    y: '<?php echo date('Y-m-d', strtotime('-' . $m . ' days')); ?>',
+                    a: <?php
+                                $in_where = "`app_id` = $app_id AND `date` BETWEEN '" . $nowmonth . "' AND '" . $lastmonth . "'";
 
-    let install_labels = ['Installs']
-    let install_colors = ['#E7C00B']
+                                $installed = $this->db->where('event', 'installed')->where($where)->get('shopify_data')->num_rows();
+                                $reactivated = $this->db->where('event', 'reactivated')->where($where)->get('shopify_data')->num_rows();
+                                $uninstalled = $this->db->where('event', 'uninstalled')->where($where)->get('shopify_data')->num_rows();
+                                $deactivated = $this->db->where('event', 'deactivated')->where($where)->get('shopify_data')->num_rows();
 
-    let uninstall_labels = ['Uninstalls']
-    let uninstall_colors = ['#D05421']
+                                $lost = ($uninstalled + $deactivated);
+                                $gained = ($installed + $reactivated);
 
-    let churn_labels = ['Churn (%)']
-    let churn_colors = ['#71DFCC']
+                                if ($lost == '' || $lost == 0 || $gained == '' || $gained == 0) {
+                                    $total = 0;
+                                } else {
+                                    $total = (($lost / $gained) * 100);
+                                }
+                                echo number_format($total);
+                        ?>
+                },
+            <?php endfor; ?>
+        ];
 
-    let reviews_keys = ['a', 'b', 'c', 'd', 'e']
-    let reviews_labels = ['5 star', '4 star', '3 star', '2 star', '1 star']
-    let reviews_colors = ['#D05421', '#21D1B1', '#C90100', '#E7C00B', '#1E1E1E']
+        let reviews_data = [<?php for ($m = 7; $m > 0; $m--) :
+                                $nowmonth = strtotime(date('d-M-Y', strtotime('-' . $m . ' days')));
+                                $lastmonth = strtotime(date('d-M-Y', strtotime('-' . ($m - 1) . ' days'))); ?> {
+                    y: '<?php echo date('Y-m-d', strtotime('-' . $m . ' days')); ?>',
+                    a: <?php
+                                $where = "`app_id` = $app_id AND `rating` = 5 AND `review_date` BETWEEN '" . $nowmonth . "' AND '" . $lastmonth . "'";
+                                $reviews = $this->db->where($where)->get('reviews')->num_rows();
+                                echo $reviews;
+                        ?>,
+                    b: <?php
+                                $where = "`app_id` = $app_id AND `rating` = 4 AND `review_date` BETWEEN '" . $nowmonth . "' AND '" . $lastmonth . "'";
+                                $reviews = $this->db->where($where)->get('reviews')->num_rows();
+                                echo $reviews;
+                        ?>,
+                    c: <?php
+                                $where = "`app_id` = $app_id AND `rating` = 3 AND `review_date` BETWEEN '" . $nowmonth . "' AND '" . $lastmonth . "'";
+                                $reviews = $this->db->where($where)->get('reviews')->num_rows();
+                                echo $reviews;
+                        ?>,
+                    d: <?php
+                                $where = "`app_id` = $app_id AND `rating` = 2 AND `review_date` BETWEEN '" . $nowmonth . "' AND '" . $lastmonth . "'";
+                                $reviews = $this->db->where($where)->get('reviews')->num_rows();
+                                echo $reviews;
+                        ?>,
+                    e: <?php
+                                $where = "`app_id` = $app_id AND `rating` = 1 AND `review_date` BETWEEN '" . $nowmonth . "' AND '" . $lastmonth . "'";
+                                $reviews = $this->db->where($where)->get('reviews')->num_rows();
+                                echo $reviews;
+                        ?>
+                },
+            <?php endfor; ?>
+        ];
+
+        let revenue_keys = ['a']
+        let revenue_labels = ['Revenue']
+        let revenue_colors = ['#1D47F1']
+
+        let user_keys = ['a']
+        let user_labels = ['User Growth']
+        let user_colors = ['#C90100']
+
+        let install_labels = ['Installs']
+        let install_colors = ['#E7C00B']
+
+        let uninstall_labels = ['Uninstalls']
+        let uninstall_colors = ['#D05421']
+
+        let churn_labels = ['Churn (%)']
+        let churn_colors = ['#71DFCC']
+
+        let reviews_keys = ['a', 'b', 'c', 'd', 'e']
+        let reviews_labels = ['5 star', '4 star', '3 star', '2 star', '1 star']
+        let reviews_colors = ['#D05421', '#21D1B1', '#C90100', '#E7C00B', '#1E1E1E']
 
 
 
 
 
-    jQuery(document).ready(function($) {
         drawLine('revenue_chart', revenue_data, revenue_keys, revenue_labels, revenue_colors)
         drawBar('users_chart', user_data, user_keys, user_labels, user_colors)
         drawLine('installs_chart', installs_data, revenue_keys, install_labels, install_colors)

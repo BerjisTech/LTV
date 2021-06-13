2ef0b47d5a618d192b411f7ef45ceb6da8fa1fbb
berjis <berjistechnologies@gmail.com>
2021-06-13 21:39:24 +0300

    Transactions from pp_financials

--- application/controllers/Ltv.php
+++ application/controllers/Ltv.php
@@ -445,7 +445,7 @@ class Ltv extends CI_Controller
     public function kwengport_from_file($file)
     {
         $file = base_url("data/$file.csv");
-        $sql = "LOAD DATA 
+        $sql = "LOAD DATA LOCAL
         LOW_PRIORITY 
         INFILE '$file'
         INTO TABLE `app_users` 
